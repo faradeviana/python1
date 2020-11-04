819e73a3ea6dc2772e3de23d6c88e1cb83f0bfd2
faradeviana <72803399+faradeviana@users.noreply.github.com>
2020-11-04 05:31:51 -0500

    Add files via upload

---
+++ FARA.py
@@ -0,0 +1,17 @@
+print "  ====================================" 
+print "       Latihan 1 Biodata Rizky        "
+print "  ===================================="
+
+#variabel
+
+nama= raw_input ("Masukan Nama Lengkap Anda: ")
+panggilan= raw_input ("Masukan Nama Panggilan: ")
+nim= raw_input ("Masukan Nim Anda: ")
+ttl= raw_input ("Masukan Tempat Lahir Anda`: ")
+tl= input ("Masukan Tanggal lahir  Anda: ")
+alamat= raw_input ("Masukan Alamat Anda: ")
+telpone= raw_input ("Masukan No Telpon Anda: ")
+
+#hasil inputan dari variable
+print "Assalamu'alaikum Wr.Wb"
+print "Let me introduce my self My name is",nama,"but you can call me",panggilan,"my NPM is",nim,"I was born in",ttl,"and I am",tl,"years old. I am very glad if you want to invite my house in",alamat,".So, don't forget to call me before with the number",telpone,
